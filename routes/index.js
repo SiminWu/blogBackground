36940df732c5ec63d28b95fb0fbfcd59b5cd1038
吴思敏 <wsm2@xlbzone.com>
2017-12-14 18:33:40 +0800

    数据连接

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ var router = express.Router();
 var sqlSearch = require('./server/sqlSearch');
 // console.log(sqlSearch)
 /* GET home page. */
+//设置跨域访问
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+    next();
 });
+
 router.get('/login', function(req, res, next) {
     console.log("--")
-    // console.log(req);
-    // console.log(res);
-    console.log("++")
-    sqlSearch.query()
+    console.log(req);
+    console.log(res);
+    console.log("++");
+    res.send('root');
+    // sqlSearch.query()
 
 });
 module.exports = router;
